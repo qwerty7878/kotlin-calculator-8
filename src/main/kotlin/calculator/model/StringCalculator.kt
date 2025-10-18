82ba7b1c3dd75c5b32a4eac0a77127fbd37750f6
hyeonjune <annhj980@naver.com>
2025-10-18 13:15:16 +0900

    feat: 커스텀 구분자 유무 판별 구현

--- src/main/kotlin/calculator/model/StringCalculator.kt
+++ src/main/kotlin/calculator/model/StringCalculator.kt
@@ -1,12 +1,34 @@
 package calculator.model
 
+const val PREFIX_CUSTOM_DELIMETER = "//"
+const val POSTFIX_CUSTOM_DELIMETER = "\n"
+const val FORMAT_ERROR_MESSAGE = "올바른 형식이 아닙니다."
+
 class StringCalculator {
     fun calculate(input: String?): Int {
 //        0 구분 여부
-        if (input.isNullOrEmpty()) {
-            return 0
+        if (input.isNullOrEmpty()) return 0
+//        문자열 파싱
+//        커스텀 구분자구분
+        if (input.startsWith(PREFIX_CUSTOM_DELIMETER)) {
+            extractCustmDelimeter(input)
         }
-//        return 문자열 구분자 처리 로직
+//        커스텀 구분자라면 형식 올바른지 확인하고 구분자 확인 후 구분자에 따라 배열 쪼개기
+//        기본 구분자라면 구분자에 따라 쪼개기
         return 0
+    }
+
+    private fun extractCustmDelimeter(input: String): String {
+        val customDelimeterIndex = input.indexOf(POSTFIX_CUSTOM_DELIMETER)
+        formatValidation(customDelimeterIndex)
+
+        val customDelimeter = input.substring(2, customDelimeterIndex)
+        return customDelimeter
+    }
+
+//      "//"로 시작하고 "\n"이 없는 형식 오류 판별
+//      자체적으로 IllegalArgumentException를 던짐
+    private fun formatValidation(index: Int) {
+        require(index != -1) { FORMAT_ERROR_MESSAGE }
     }
 }
--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -3,6 +3,7 @@ package calculator
 import calculator.model.StringCalculator
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CalculatorTest {
     private val calculator = StringCalculator()
@@ -15,5 +16,56 @@ class CalculatorTest {
     @Test
     fun `null 입력 시 0 반환`() {
         assertEquals(0, calculator.calculate(null))
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 슬래시가 하나인 경우`() {
+//        기본 구분자로 분류가 됨 바로 정수변환이 되는게 아닌 '/' 이 포함되어 있기 때문에 이를 처리할 로직을 구현해야함
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("/;\\n1;2;3")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 커스텀 구분자가 없는 경우`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("//\\n1;2;3")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 여러개의 앞 구분자만 있는 경우`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("////\\n1;2;3")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 여러개의 뒷 구분자가 있는 경우`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("//")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 앞 구분자만 있는 경우`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("//\\n\\n1;2;3")
+        }
+    }
+
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 뒷 구분자가 없는 경우`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("//;1;2;3")
+        }
+    }
+
+    @Test
+    fun `커스텀 구분자 형식이 올바르지 않은 경우 예외처리 통과`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.calculate("//;\\n1;2;3")
+        }
     }
 }
