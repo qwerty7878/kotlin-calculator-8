3fd8aeaaf7d70efc897194e0421945ff33ade864
hyeonjune <annhj980@naver.com>
2025-10-18 14:15:31 +0900

    feat: 커스텀 구분자로 문자열 분리 기능 구현

--- src/main/kotlin/calculator/model/StringCalculator.kt
+++ src/main/kotlin/calculator/model/StringCalculator.kt
@@ -6,39 +6,62 @@ const val FORMAT_ERROR_MESSAGE = "올바른 형식이 아닙니다."
 const val DEFAULT_DELIMETER_COMMA = ","
 const val DEFAULT_DELIMETER_COLON = ":"
 
-
 class StringCalculator {
     fun calculate(input: String?): Int {
-//        0 구분 여부
         if (input.isNullOrEmpty()) return 0
-//        문자열 파싱
-//        커스텀 구분자구분
-        if (input.startsWith(PREFIX_CUSTOM_DELIMETER)) {
-            extractCustmDelimeter(input)
-        }
-//        커스텀 구분자라면 형식 올바른지 확인하고 구분자 확인 후 구분자에 따라 배열 쪼개기
-//        기본 구분자라면 구분자에 따라 쪼개기
-        defaultArray(input)
-        return 0
+        if (input.startsWith(PREFIX_CUSTOM_DELIMETER))
+            return calculateCustomDelimeter(input)
+        return calculateDefaultDelimeter(input)
     }
 
-    private fun extractCustmDelimeter(input: String): String {
-        val customDelimeterIndex = input.indexOf(POSTFIX_CUSTOM_DELIMETER)
+    private fun calculateDefaultDelimeter(input: String): Int {
+        val defaultArray = splitDefaultDelimeter(input)
+        return sumList(defaultArray)
+    }
+
+    private fun calculateCustomDelimeter(input: String): Int{
+        val customDelimeterIndex = findEndIndex(input)
         formatValidation(customDelimeterIndex)
 
-        val customDelimeter = input.substring(2, customDelimeterIndex)
-        return customDelimeter
+        val customDelimeter = extractCustmDelimeter(input, customDelimeterIndex)
+        val numberPart = extractNumberPart(input, customDelimeterIndex)
+
+//        커스텀 구분자에 따른 문자열 분리
+        val customArray = splitCustomDelimeter(numberPart, customDelimeter)
+        return sumList(customArray)
+    }
+
+//    커스텀 구분자 파악을 위한 인덱스 추출
+//    '\n'보다 한 칸 앞에 위치해 있으므로
+//    만약 인덱스가 없어서 -1이라면 예외처리를 위해
+    private fun findEndIndex(input: String): Int {
+        return input.indexOf(POSTFIX_CUSTOM_DELIMETER)
+    }
+
+//    커스텀 구분자 추출
+    private fun extractCustmDelimeter(input: String, endIndex: Int): String {
+        return input.substring(2, endIndex)
+    }
+
+//    커스텀 구분자 이후 숫자 부분 추출
+    private fun extractNumberPart(input: String, endIndex: Int): String {
+        return input.substring(endIndex + 1)
+    }
+
+//    커스텀 배열 쪼개기
+    private fun splitCustomDelimeter(input: String, customDelimeter: String): List<String> {
+        return input.split(customDelimeter)
+    }
+
+//    기본 구분자에 따른 문자열 분리
+    private fun splitDefaultDelimeter(input: String): List<String> {
+        val delimeterArray = arrayOf(DEFAULT_DELIMETER_COLON, DEFAULT_DELIMETER_COMMA)
+        return input.split(*delimeterArray)
     }
 
 //      "//"로 시작하고 "\n"이 없는 형식 오류 판별
 //      자체적으로 IllegalArgumentException를 던짐
     private fun formatValidation(index: Int) {
         require(index != -1) { FORMAT_ERROR_MESSAGE }
-    }
-
-//    기본 구분자에 따른 문자열 분리
-    private fun defaultArray(input: String): List<String> {
-        val delimeterArray = arrayOf(DEFAULT_DELIMETER_COLON, DEFAULT_DELIMETER_COMMA)
-        return input.split(*delimeterArray)
     }
 }
